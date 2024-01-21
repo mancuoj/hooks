e370d51e9959fabd359ccb27cf568d3f5d572ac2
mancuoj <1337381768@qq.com>
2024-01-21 20:38:07 +0800

    test: add more tests

--- src/tests/use-counter.test.ts
+++ src/tests/use-counter.test.ts
@@ -1,14 +1,61 @@
 import { describe, expect, it } from 'vitest'
 import { renderHook } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
-import useCounter from '../hooks/use-counter'
+import { useCounter } from '../hooks/use-counter'
 
 describe('useCounter', () => {
-  it('should increment', () => {
+  it('should use counter', () => {
     const { result } = renderHook(() => useCounter())
+    expect(result.current.count).toBe(0)
+    expect(result.current.inc).toBeTypeOf('function')
+    expect(result.current.dec).toBeTypeOf('function')
+    expect(result.current.reset).toBeTypeOf('function')
+    expect(result.current.setCount).toBeTypeOf('function')
+  })
+
+  it('should default value works', () => {
+    const { result } = renderHook(() => useCounter(10))
+    expect(result.current.count).toBe(10)
+  })
+
+  it('should increment counter', () => {
+    const { result } = renderHook(() => useCounter(1))
     act(() => {
       result.current.inc()
     })
-    expect(result.current.count).toBe(1)
+    expect(result.current.count).toBe(2)
+  })
+
+  it('should decrement counter', () => {
+    const { result } = renderHook(() => useCounter())
+    act(() => {
+      result.current.dec()
+    })
+    expect(result.current.count).toBe(-1)
+  })
+
+  it('should set counter', () => {
+    const { result } = renderHook(() => useCounter())
+    act(() => {
+      result.current.setCount(10)
+    })
+    expect(result.current.count).toBe(10)
+  })
+
+  it('should reset counter', () => {
+    const { result } = renderHook(() => useCounter())
+    act(() => {
+      result.current.setCount(10)
+      result.current.reset()
+    })
+    expect(result.current.count).toBe(0)
+  })
+
+  it('should set counter use previous value', () => {
+    const { result } = renderHook(() => useCounter(3))
+    act(() => {
+      result.current.setCount(prev => prev + 7)
+    })
+    expect(result.current.count).toBe(10)
   })
 })
