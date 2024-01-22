ee75a10619cf87e0a7cb917561d35824cd25bb63
mancuoj <1337381768@qq.com>
2024-01-23 00:17:08 +0800

    test: clean

--- src/hooks/use-counter.test.ts
+++ src/hooks/use-counter.test.ts
@@ -6,7 +6,6 @@ import { useCounter } from './use-counter'
 describe('use counter', () => {
   it('should use counter', () => {
     const { result } = renderHook(() => useCounter())
-
     expect(result.current.count).toBe(0)
     expect(result.current.inc).toBeTypeOf('function')
     expect(result.current.dec).toBeTypeOf('function')
@@ -14,42 +13,31 @@ describe('use counter', () => {
     expect(result.current.setCount).toBeTypeOf('function')
   })
 
-  it('should default value works', () => {
+  it('should use default value', () => {
     const { result } = renderHook(() => useCounter(10))
-
     expect(result.current.count).toBe(10)
   })
 
   it('should increment counter', () => {
     const { result } = renderHook(() => useCounter(1))
-
-    act(() => {
-      result.current.inc()
-    })
+    act(() => result.current.inc())
     expect(result.current.count).toBe(2)
   })
 
   it('should decrement counter', () => {
     const { result } = renderHook(() => useCounter())
-
-    act(() => {
-      result.current.dec()
-    })
+    act(() => result.current.dec())
     expect(result.current.count).toBe(-1)
   })
 
   it('should set counter', () => {
     const { result } = renderHook(() => useCounter(2))
-
-    act(() => {
-      result.current.setCount(10)
-    })
+    act(() => result.current.setCount(10))
     expect(result.current.count).toBe(10)
   })
 
   it('should reset counter', () => {
     const { result } = renderHook(() => useCounter())
-
     act(() => {
       result.current.setCount(10)
       result.current.reset()
@@ -59,10 +47,7 @@ describe('use counter', () => {
 
   it('should set counter use previous value', () => {
     const { result } = renderHook(() => useCounter(3))
-
-    act(() => {
-      result.current.setCount(prev => prev + 7)
-    })
+    act(() => result.current.setCount(prev => prev + 7))
     expect(result.current.count).toBe(10)
   })
 })
